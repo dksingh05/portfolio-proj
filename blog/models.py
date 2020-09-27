aa0050f1b0abb1a219649b71f688d2f411a4b6a8
Dhananjay Singh <dksingh05@gmail.com>
2020-09-27 14:50:56 +0530

    jobs are showing up

--- blog/models.py
+++ blog/models.py
@@ -2,3 +2,7 @@ from django.db import models
 
 class Blog(models.Model):
     title = models.CharField(max_length=200)
+    pub_date = models.DateTimeField(null=True)
+    body = models.TextField(max_length=1000, default='SOME STRING')
+    image = models.ImageField(upload_to='images/', null=True)
+
